2dd02e036d8de61108abaffd6d9c2d96ef08ee9e
Kevin Chan <kevin.chan@kingwaytek.com>
2020-08-27 14:35:04 +0800

    1. fix crash bug 2. fix Localizable bug

--- Cocktail/Model/Alert.swift
+++ Cocktail/Model/Alert.swift
@@ -12,19 +12,6 @@ import Moya
 
 func alert(error: Error) -> UIAlertController {
 
-    let nsError = error as NSError
-    if nsError.domain == NSURLErrorDomain {
-        return alert(error: nsError)
-    }
-
-    if let decodingError = error as? DecodingError {
-        return alert(error: decodingError)
-    }
-
-    if let moyaError = error as? MoyaError {
-        return alert(error: moyaError)
-    }
-
     let controller = UIAlertController(title: NSLocalizedString("UI.Failed", comment: "Fail"), message: error.localizedDescription, preferredStyle: .alert)
     controller.addAction(UIAlertAction(title: NSLocalizedString("UI.OK", comment: "OK"), style: .cancel, handler: nil))
 
